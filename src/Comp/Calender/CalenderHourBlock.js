d56597a61a302e9cf41258514655bc412ec1d77a
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2023-01-07 14:51:04 -0700

    Add Events to Calender
    Per Date / Time

--- src/Comp/Calender/CalenderHourBlock.js
+++ src/Comp/Calender/CalenderHourBlock.js
@@ -1,7 +1,9 @@
 import React from "react";
+import CalenderEventBlock from "./CalenderEventBlock";
 
 export default function CalenderHourBlock({ time, events, date }) {
   let dailyEvent = [];
+  let data;
 
   events.map((item, index) => {
     // console.log(time.substr(0, 2));
@@ -11,24 +13,25 @@ export default function CalenderHourBlock({ time, events, date }) {
 
     if (time.substr(0, 2) === item.Time.substr(0, 2) && date === item.Date) {
       dailyEvent.push(item);
-      dailyEvent.sort(function (a, b) {
-        return a.Time.substr(2, 2).localCompare(b.Time.substr(2, 2));
-      });
-      console.log(dailyEvent);
+      dailyEvent.sort((a, b) => a.Time.localeCompare(b.Time));
+      data = dailyEvent[0].Event;
     }
     return null;
   });
 
-  // console.log(dailyEvent);
+  console.log(dailyEvent);
 
-  let data = "";
   // console.log(dailyEvent[0].Event || false);
 
   return (
     <div className="max-w-full h-auto m-2 bg-orange-500">
       <div className="flex flex-row w-full border-b-2 border-black">
         <div className="h-6">{time}</div>
-        <div className="min-h-min flex flex-col">{data}</div>
+      </div>
+      <div className="min-h-min w-full flex flex-col">
+        {dailyEvent.map((items, index) => {
+          return <CalenderEventBlock key={index} />;
+        })}
       </div>
     </div>
   );
